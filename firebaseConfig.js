3a2935b2e689cc5dd3e38fff2c3724657deb307a
Weerawardana pathirannehelage anurudda dilip kumar weerawardana <sathyadarshana1@gmail.com>
2025-07-20 19:18:06 +0530

    Update firebaseConfig.js
    Update firebaseConfig.js with revised Firebase configuration

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,10 @@
-// Replace with your config from Firebase Console
+// firebaseConfig.js
 export const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyCKWTVsWeE7YbEkfTihyijVJo43GLXXtc",
+  authDomain: "sathyadarshanaai.firebaseapp.com",
+  projectId: "sathyadarshanaai",
+  storageBucket: "sathyadarshanaai.appspot.com",
+  messagingSenderId: "344390555524",
+  appId: "1:344390555524:web:e2af9b3c3dfa1bb6232fd1",
+  measurementId: "G-TVSH8LDZE2"
 };

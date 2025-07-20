cf642cb0210027ac941a3e4127709c0bc41202bb
Weerawardana pathirannehelage anurudda dilip kumar weerawardana <sathyadarshana1@gmail.com>
2025-07-20 16:39:40 +0530

    Create firebaseConfig.js
    Add firebaseConfig.js with Firebase project configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+// Replace with your config from Firebase Console
+export const firebaseConfig = {
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "..."
+};

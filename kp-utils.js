8f63588a989ddee039797c85c662806a09c8d925
Weerawardana pathirannehelage anurudda dilip kumar weerawardana <sathyadarshana1@gmail.com>
2025-07-31 23:16:12 +0530

    Update kp-utils.js
    Add getSubSubLord function for KP SubSub Lord calculation

--- kp-utils.js
+++ kp-utils.js
@@ -17,4 +17,19 @@ function getSubSubLord(nakshatraStartDeg, userDeg) {
   // 1. Find Sub Lord range (using dasha year % of arc)
   // 2. Inside that sub-section, find sub-sub lord (again % logic)
   // ...
+}function getSubSubLord(degree) {
+  const subs = [
+    { name: "Ketu", range: 0 },
+    { name: "Venus", range: 1.2 },
+    { name: "Sun", range: 2.4 },
+    { name: "Moon", range: 3.6 },
+    { name: "Mars", range: 4.8 },
+    { name: "Rahu", range: 6.0 },
+    { name: "Jupiter", range: 7.2 },
+    { name: "Saturn", range: 8.4 },
+    { name: "Mercury", range: 9.6 }
+  ];
+  const mod = degree % 13.3333;
+  const index = Math.floor(mod / 1.48148); // rough subdivision
+  return subs[index % subs.length].name;
 }

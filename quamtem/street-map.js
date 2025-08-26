4b3d84b0eee664a09d95c0f096f8dcfc237d9c48
Weerawardana pathirannehelage anurudda dilip kumar weerawardana <sathyadarshana1@gmail.com>
2025-08-26 17:17:28 +0530

    Refactor street-map.js for improved map interaction
    Refactor street-map.js to enhance map controls and add marker functionality.

--- quamtem/street-map.js
+++ quamtem/street-map.js
@@ -1,18 +1,18 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Street Map Test</title>
-  <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css"/>
-  <style>
-    #map { height: 180px; width: 100%; }
-  </style>
-</head>
-<body>
-  <div id="map"></div>
-  <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
-  <script>
-    const map = L.map('map').setView([7.1, 79.9], 7);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(map);
-  </script>
-</body>
-</html>
+// street-map.js
+let marker = null;
+const map = L.map('map', {
+  zoomControl: false,
+  attributionControl: false,
+  dragging: true,
+  scrollWheelZoom: false,
+  doubleClickZoom: false,
+  boxZoom: false,
+  keyboard: false,
+  tap: false
+}).setView([7.1, 79.9], 7);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(map(marker);
+  marker = L.marker(e.latlng).addTo(map);
+  document.getElementById('lat').value = e.latlng.lat.toFixed(4);
+  document.getElementById('lon').value = e.latlng.lng.toFixed(4);
+});
